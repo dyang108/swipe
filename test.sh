542cc3e29c406783db9f2f81043097338a70b530
Derick Yang <derick.yang@tufts.edu>
2016-05-24 22:53:13 -0400

    seventh try

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-npm install -g grunt --save-dev
+npm install grunt --save-dev
+npm install -g grunt
 npm install -g grunt-cli
 npm install -g jslint
+npm install grunt-lesslint
+npm install grunt-htmllint
 # ensure less is stylish
 grunt lesslint
 # to get really fancy, I should use Protractor http://www.protractortest.org/#/
